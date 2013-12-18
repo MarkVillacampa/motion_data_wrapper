fe6341f8906acef11b803c174277ca08da25ddb2
Mark Villacampa <markvjal@gmail.com>
2013-12-18 17:18:23 +0100

    This change fixes finders_spec.rb tests. Read the comment for more information.

--- lib/motion_data_wrapper/relation/core_data.rb
+++ lib/motion_data_wrapper/relation/core_data.rb
@@ -3,12 +3,30 @@ module MotionDataWrapper
     module CoreData
 
       def inspect
-         to_a
-       end
+        to_a
+      end
 
       def to_a
         error_ptr = Pointer.new(:object)
-        context.executeFetchRequest(self, error:error_ptr)
+        #
+        # The returned value from `executeFetchRequest:error:` is an NSArray
+        # Most of the time RubyMotion treats NSArray and pure ruby Arrays the same way,
+        # however in this case we want the array to be a pure ruby Array, so that array
+        # comparison can be done via its members #== method
+        #
+        # We need this so that comparison between arrays of NSManagedObjects
+        # fire our custom MotionDataWrapper::Model#== method
+        #
+        # This is the scenario when Array() is not called here:
+        #
+        # task = Task.create
+        # task == Task.all.first  => true
+        # Task.all.first == task  => true
+        # [task] == Task.all      => false
+        #
+        # Internally, Task.all is a NSArray, but Array(Task.all) is a ruby Array
+        #
+        Array(context.executeFetchRequest(self, error:error_ptr))
       end
 
       private
