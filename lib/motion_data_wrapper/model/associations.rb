07ff7d3ce6dca99d165f001705708ad485bdf75e
Mark Villacampa <markvjal@gmail.com>
2014-01-17 18:31:58 +0100

    Changed the way dynamic association methods are generated.  was causing problems when called in a CoreDara dynamic subclass

--- lib/motion_data_wrapper/model/associations.rb
+++ lib/motion_data_wrapper/model/associations.rb
@@ -14,14 +14,14 @@ module MotionDataWrapper
 
       private
       def generate_methods_for_collection_association(name, description)
-        self.define_singleton_method(name) do
+        self.class.send(:define_method, name, lambda {
           collection_proxy_cache[name] ||= CollectionProxy.new(self, name, description)
-        end
+        })
 
-        self.define_singleton_method("#{name}=") do |value|
+        self.class.send(:define_method, "#{name}=", lambda { |value|
           collection_proxy_cache[name] ||= CollectionProxy.new(self, name, description)
           collection_proxy_cache[name].writer(value)
-        end
+        })
       end
 
       def collection_proxy_cache
@@ -31,26 +31,26 @@ module MotionDataWrapper
       def generate_methods_for_to_one_association(name, description)
         # If the object we are setting has already been persisted we retrieve it in the local context.
         # If the object we are setting has not been persisted, we clone it and delete it from its local context.
-        self.define_singleton_method("#{name}=") do |value|
+        self.class.send(:define_method, "#{name}=", lambda { |value|
           value.save! if !value.persisted?
           object_in_local_context = self.managedObjectContext.objectWithID(value.objectID)
           setValue(object_in_local_context, forKey: name)
-        end
+        })
 
-        self.define_singleton_method("build_#{name}") do |attributes|
+        self.class.send(:define_method, "build_#{name}", lambda { |attributes|
           klass = Kernel.const_get(description.destinationEntity.managedObjectClassName)
           obj = klass.new_with_context(attributes, self.managedObjectContext)
           setValue(obj, forKey: name)
           obj
-        end
+        })
 
-        self.define_singleton_method("create_#{name}") do |attributes|
+        self.class.send(:define_method, "create_#{name}", lambda{ |attributes|
           klass = Kernel.const_get(description.destinationEntity.managedObjectClassName)
           obj = klass.new_with_context(attributes, self.managedObjectContext)
           setValue(obj, forKey: name)
           save!
           obj
-        end
+        })
       end
     end
   end
