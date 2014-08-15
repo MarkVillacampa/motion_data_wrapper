bedc3baac88e861e151bb7025bac3edab2792f00
Mark Villacampa <markvjal@gmail.com>
2014-08-16 01:07:25 +0200

    Handle iOS and OSX in the app delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,19 @@
 class AppDelegate
   include MotionDataWrapper::Delegate
+
+  # IOS
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    if RUBYMOTION_ENV == 'test'
+      MotionDataWrapper.clean_core_data
+    end
+    true
+  end
+
+  # OSX
+  def applicationDidFinishLaunching(notification)
+    if RUBYMOTION_ENV == 'test'
+      MotionDataWrapper.clean_core_data
+    end
+    true
   end
 end
